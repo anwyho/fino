7b715af42e795f292338dde5282d03b9adb681d8
Anthony W. Ho <anwyho@gmail.com>
2021-11-13 07:02:29 +0000

    Move logger to config and adapt to Notion logger

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,6 @@
-import * as dotenv from 'dotenv'
-import { pino } from 'pino'
 
+import { logger } from './config'
 import addItem from './helpers/notion_demo'
-
-dotenv.config()
-const logger = pino({ level: process.env.LOG_LEVEL ?? 'info' })
-logger.info('started server')
 
 // FIXME: this is purely here to boost code-coverage.
 export const callAddItem = async function (): Promise<void> {
